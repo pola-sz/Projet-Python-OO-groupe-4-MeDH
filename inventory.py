f15d94e5da1964caaf03976c5fcce570dafaaad0
Snort0Vaulter <mychabane.yc@gmail.com>
2025-11-15 22:30:44 +0100

    Add files via upload

--- inventory.py
+++ inventory.py
@@ -5,9 +5,13 @@ class Inventory :
         
         self.steps = 70
         self.coins = 0
+        self.coins_chance = 0
         self.gems = 2
+        self.gems_chance = 0
         self.keys = 2
+        self.keys_chance = 0
         self.dices = 0
+        self.dices_chance = 0
         self.object_list = Object()
 
 
--- object.py
+++ object.py
@@ -1,10 +1,15 @@
 class Object : 
     def __init__(self):
         self.shovel = False
+        self.shovel_chance = 0
         self.hammer = False
+        self.hammer_chance = 0
         self.crochet_kit = False
+        self.crochet_kit_chance = 0
         self.metal_detector = False
+        self.metal_detector_chance = 0
         self.rabbit_foot = False
+        self.rabbit_foot_chance = 0
         self.apple = 0
         self.banana = 0
         self.cake = 0
